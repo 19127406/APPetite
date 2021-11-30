bedbaa692cbed1f5481eb5cb1ac75da94c6730b7
YoRHa <snowline2015@gmail.com>
2021-11-30 20:14:09 +0700

    temp

--- APPetite/APPetite/APPetite/ForgotPasswordPage.xaml.cs
+++ APPetite/APPetite/APPetite/ForgotPasswordPage.xaml.cs
@@ -16,5 +16,31 @@ namespace APPetite
         {
             InitializeComponent();
         }
+
+        private async void Go_back_clicked(object sender, EventArgs e)
+        {
+            await Application.Current.MainPage.Navigation.PopAsync();
+        }
+
+        private void checkToEnable()
+        {
+            ResetPasswordButton.IsEnabled = false;
+            ResetPasswordButton.Background = Brush.DarkGray;
+            if (!string.IsNullOrEmpty(forgotEmailEntry.Text))
+            {
+                ResetPasswordButton.IsEnabled = true;
+                ResetPasswordButton.Background = Brush.LightGray;
+            }
+        }
+
+        void entryTextChanged(object sender, Xamarin.Forms.TextChangedEventArgs e)
+        {
+            checkToEnable();
+        }
+
+        private void Reset_password_clicked(object sender, EventArgs e)
+        {
+
+        }
     }
 }
