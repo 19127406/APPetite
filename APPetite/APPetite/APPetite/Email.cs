465929bd3ecd9fbae48f6d4b34b726d82bf504e8
YoRHa <snowline2015@gmail.com>
2021-12-01 20:16:17 +0700

    ok

--- APPetite/APPetite/APPetite/Email.cs
+++ APPetite/APPetite/APPetite/Email.cs
@@ -21,86 +21,7 @@ namespace APPetite
             mail.Subject = subject;
 
             mail.IsBodyHtml = true;
-            string htmlString = @"
-                    <!DOCTYPE html>
-                    <html lang=""en"">
-                    <head>
-                        <meta charset=""UTF-8"">
-                        <meta http-equiv=""X-UA-Compatible"" content=""IE=edge"">
-                        <meta name=""viewport"" content=""width=device-width, initial-scale=1.0"">
-                        <title>Document</title>
-                        <style>
-                            @import url('https://fonts.googleapis.com/css2?family=Montserrat:400,800');
-                            *{
-                                box-sizing: border-box;
-                            }
-                            body{
-                                font-family: 'Montserrat', sans-serif;
-                                display: flex;
-                                flex-direction: row;
-                                justify-content: center;
-                                align-items: center;
-                                background-color: #f6f5f7;
-                            }
-                            #passcode{
-                                color: #5777f2;
-                            }
-                            .container{
-                                background-color: #fff;
-                                position: relative;
-                                overflow: hidden;
-                                box-shadow: 5px 5px 5px rgba(116, 115, 115, 0.1);
-                                width: 50%;
-                            }
-                            .inner-container{
-                                background: #fff;
-                                display: flex;
-                                flex-direction: column;
-                                justify-content: center;
-                                align-items: center;
-                                margin: 3rem 0;
-                            }
-                            .inner-container p{
-                                font-size: .9rem;
-                                color: gray;
-                                width: 70%;
-                                height: 100%;
-                                text-align: center;
-                            }
-                            .top-container{
-                                color: rgb(95, 95, 95);
-                                width: 70%;
-                                height: 100%;
-                                position: relative;
-                                display: flex;
-                                flex-direction: column;
-                                justify-content: center;
-                                align-items: center;
-                                text-align: center;
-                            }
-                        </style>
-                    </head>
-                    <body>
-                        <div class=""container"">
-                            <div class=""inner-container"">
-                                <div class=""top-container"">
-                                    <h1>Password Reset</h1>
-                                    <h3>If you've lost your password or wish to reset it, here is the code to identify it's you</h3>
-                                </div>
-                                <h1 id=""passcode"">code</h1>
-                                <p>If you did not request a password reset, you can safely ignore this email. Only a person with access to your email can reset your account password</p>
-                            </div>
-                        </div>
-                        <script>
-                            function GenerateRandom(length){
-                                return Math.floor(Math.pow(10, length-1) + Math.random() * 9 * Math.pow(10, length-1));
-                            }
-                            let code = document.getElementById(""passcode"");
-                            code.innerHTML = GenerateRandom(8).toString();
-                        </script>
-                    </body>
-                    </html>
-                     ";
+            string htmlString = html_reset_password();
 
             mail.Body = htmlString;
 
@@ -110,6 +31,256 @@ namespace APPetite
             SmtpServer.EnableSsl = true;
 
             SmtpServer.Send(mail);
+        }
+
+        private string html_reset_password()
+        {
+            string html = @"<!DOCTYPE html>
+<html>
+<head>
+
+  <meta charset=""utf-8"">
+  <meta http-equiv=""x-ua-compatible"" content=""ie=edge"">
+  <title>Password Reset</title>
+  <meta name=""viewport"" content=""width=device-width, initial-scale=1"">
+  <style type=""text/css"">
+  /**
+   * Google webfonts. Recommended to include the .woff version for cross-client compatibility.
+   */
+  @media screen {
+    @font-face {
+      font-family: 'Source Sans Pro';
+      font-style: normal;
+      font-weight: 400;
+      src: local('Source Sans Pro Regular'), local('SourceSansPro-Regular'), url(https://fonts.gstatic.com/s/sourcesanspro/v10/ODelI1aHBYDBqgeIAH2zlBM0YzuT7MdOe03otPbuUS0.woff) format('woff');
+    }
+
+    @font-face {
+      font-family: 'Source Sans Pro';
+      font-style: normal;
+      font-weight: 700;
+      src: local('Source Sans Pro Bold'), local('SourceSansPro-Bold'), url(https://fonts.gstatic.com/s/sourcesanspro/v10/toadOcfmlt9b38dHJxOBGFkQc6VGVFSmCnC_l7QZG60.woff) format('woff');
+    }
+  }
+
+  /**
+   * Avoid browser level font resizing.
+   * 1. Windows Mobile
+   * 2. iOS / OSX
+   */
+  body,
+  table,
+  td,
+  a {
+    -ms-text-size-adjust: 100%; /* 1 */
+    -webkit-text-size-adjust: 100%; /* 2 */
+  }
+
+  /**
+   * Remove extra space added to tables and cells in Outlook.
+   */
+  table,
+  td {
+    mso-table-rspace: 0pt;
+    mso-table-lspace: 0pt;
+  }
+
+  /**
+   * Better fluid images in Internet Explorer.
+   */
+  img {
+    -ms-interpolation-mode: bicubic;
+  }
+
+  /**
+   * Remove blue links for iOS devices.
+   */
+  a[x-apple-data-detectors] {
+    font-family: inherit !important;
+    font-size: inherit !important;
+    font-weight: inherit !important;
+    line-height: inherit !important;
+    color: inherit !important;
+    text-decoration: none !important;
+  }
+
+  /**
+   * Fix centering issues in Android 4.4.
+   */
+  div[style*=""margin: 16px 0;""] {
+    margin: 0 !important;
+  }
+
+  body {
+    width: 100% !important;
+    height: 100% !important;
+    padding: 0 !important;
+    margin: 0 !important;
+  }
+
+  /**
+   * Collapse table borders to avoid space between cells.
+   */
+  table {
+    border-collapse: collapse !important;
+  }
+
+  a {
+    color: #1a82e2;
+  }
+
+  img {
+    height: auto;
+    line-height: 100%;
+    text-decoration: none;
+    border: 0;
+    outline: none;
+  }
+  </style>
+
+</head>
+<body style=""background-color: #e9ecef;"">
+
+  <!-- start body -->
+  <table border=""0"" cellpadding=""0"" cellspacing=""0"" width=""100%"">
+
+    <!-- start logo -->
+    <tr>
+      <td align=""center"" bgcolor=""#e9ecef"">
+        <!--[if (gte mso 9)|(IE)]>
+        <table align=""center"" border=""0"" cellpadding=""0"" cellspacing=""0"" width=""600"">
+        <tr>
+        <td align=""center"" valign=""top"" width=""600"">
+        <![endif]-->
+        <table border=""0"" cellpadding=""0"" cellspacing=""0"" width=""100%"" style=""max-width: 600px;"">
+          <tr>
+            <td align=""center"" valign=""top"" style=""padding: 36px 24px;"">
+                <img src=""https://drive.google.com/uc?export=download&id=1LA9szQYuoKtUNyLyxPaLma8FlVIK_wG6"" style=""display: block; width: 350px; max-width: 512px; min-width: 48px;"">
+            </td>
+          </tr>
+        </table>
+        <!--[if (gte mso 9)|(IE)]>
+        </td>
+        </tr>
+        </table>
+        <![endif]-->
+      </td>
+    </tr>
+    <!-- end logo -->
+
+    <!-- start hero -->
+    <tr>
+      <td align=""center"" bgcolor=""#e9ecef"">
+        <!--[if (gte mso 9)|(IE)]>
+        <table align=""center"" border=""0"" cellpadding=""0"" cellspacing=""0"" width=""600"">
+        <tr>
+        <td align=""center"" valign=""top"" width=""600"">
+        <![endif]-->
+        <table border=""0"" cellpadding=""0"" cellspacing=""0"" width=""100%"" style=""max-width: 600px;"">
+          <tr>
+            <td align=""left"" bgcolor=""#ffffff"" style=""padding: 36px 24px 0; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; border-top: 3px solid #d4dadf;"">
+              <h1 style=""margin: 0; font-size: 32px; font-weight: 700; letter-spacing: -1px; line-height: 48px;"">Password Reset</h1>
+            </td>
+          </tr>
+        </table>
+        <!--[if (gte mso 9)|(IE)]>
+        </td>
+        </tr>
+        </table>
+        <![endif]-->
+      </td>
+    </tr>
+    <!-- end hero -->
+
+    <!-- start copy block -->
+    <tr>
+      <td align=""center"" bgcolor=""#e9ecef"">
+        <!--[if (gte mso 9)|(IE)]>
+        <table align=""center"" border=""0"" cellpadding=""0"" cellspacing=""0"" width=""600"">
+        <tr>
+        <td align=""center"" valign=""top"" width=""600"">
+        <![endif]-->
+        <table border=""0"" cellpadding=""0"" cellspacing=""0"" width=""100%"" style=""max-width: 600px;"">
+
+          <!-- start copy -->
+          <tr>
+            <td align=""left"" bgcolor=""#ffffff"" style=""padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;"">
+              <p style=""margin: 0;"">If you've lost your password or wish to reset it, here is a new password for you.</p>
+            </td>
+          </tr>
+          <!-- end copy -->
+
+          <!-- start button -->
+          <tr>
+            <td align=""left"" bgcolor=""#ffffff"">
+              <table border=""0"" cellpadding=""0"" cellspacing=""0"" width=""100%"">
+                <tr>
+                  <td align=""center"" bgcolor=""#ffffff"" style=""padding: 12px;"">
+                    <table border=""0"" cellpadding=""0"" cellspacing=""0"">
+                      <tr>
+                        <td align=""center"" bgcolor=""#1a82e2"" style=""border-radius: 6px;"">
+                          <a href=""https://sendgrid.com"" target=""_blank"" style=""display: inline-block; padding: 16px 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px;"">Do Something Sweet</a>
+                        </td>
+                      </tr>
+                    </table>
+                  </td>
+                </tr>
+              </table>
+            </td>
+          </tr>
+          <!-- end button -->
+
+          <!-- start copy -->
+          <tr>
+            <td align=""left"" bgcolor=""#ffffff"" style=""padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;"">
+              <p style=""margin: 0;"">If you did not request a password reset, you can safely ignore this email. Only a person with access to your email can reset your account password</p>
+            </td>
+          </tr>
+          <!-- end copy -->
+
+          <!-- start copy -->
+          <tr>
+            <td align=""left"" bgcolor=""#ffffff"" style=""padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px; border-bottom: 3px solid #d4dadf"">
+              <p style=""margin: 0;"">Cheers,<br> APPetite</p>
+            </td>
+          </tr>
+          <!-- end copy -->
+
+        </table>
+        <!--[if (gte mso 9)|(IE)]>
+        </td>
+        </tr>
+        </table>
+        <![endif]-->
+      </td>
+    </tr>
+    <!-- end copy block -->
+
+    <!-- start footer -->
+    <tr>
+      <td align=""center"" bgcolor=""#e9ecef"" style=""padding: 24px;"">
+        <!--[if (gte mso 9)|(IE)]>
+        <table align=""center"" border=""0"" cellpadding=""0"" cellspacing=""0"" width=""600"">
+        <tr>
+        <td align=""center"" valign=""top"" width=""600"">
+        <![endif]-->
+        <table border=""0"" cellpadding=""0"" cellspacing=""0"" width=""100%"" style=""max-width: 600px;"">
+        </table>
+        <!--[if (gte mso 9)|(IE)]>
+        </td>
+        </tr>
+        </table>
+        <![endif]-->
+      </td>
+    </tr>
+    <!-- end footer -->
+
+  </table>
+  <!-- end body -->
+
+</body>
+</html>";
+        return html;
         }
     }
 }
