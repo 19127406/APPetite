80dc2d296117813b0b88469204990c472e2afae1
HuyHoang19127406 <63275057+HuyHoang19127406@users.noreply.github.com>
2021-12-04 00:23:36 +0700

    home page

--- APPetite/APPetite/APPetite/AssemblyInfo.cs
+++ APPetite/APPetite/APPetite/AssemblyInfo.cs
@@ -1,3 +1,9 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+[assembly: ExportFont("Montserrat-Light.ttf", Alias = "Montserrat-Light")]
+[assembly: ExportFont("Montserrat-Regular.ttf", Alias = "Montserrat-Regular")]
+[assembly: ExportFont("Montserrat-Medium.ttf", Alias = "Montserrat-Medium")]
+[assembly: ExportFont("Montserrat-SemiBold.ttf", Alias = "Montserrat-SemiBold")]
