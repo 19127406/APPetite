9746b4a7b76f88a5a5b9ed9ab493e0191bd87c78
YoRHa <snowline2015@gmail.com>
2022-01-17 00:35:32 +0700

    temp

--- APPetite/APPetite/APPetite/Views/MyRecipePage.xaml.cs
+++ APPetite/APPetite/APPetite/Views/MyRecipePage.xaml.cs
@@ -1,7 +1,9 @@
-﻿using APPetite.ViewModels;
+﻿using APPetite.Models;
+using APPetite.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -113,6 +115,41 @@ namespace APPetite.Views
             }
 
             BindingContext = myRecipe;
+        }
+
+        public async void OpenRecipeDetail(object sender, EventArgs e)
+        {
+            var args = (TappedEventArgs)e;
+            var myObject = args.Parameter;
+
+            //Frame frame = (Frame)sender;
+            //TapGestureRecognizer tap = (TapGestureRecognizer)frame.GestureRecognizers[0];
+            //var test = tap.CommandParameter;
+
+            Recipe rep = (Recipe)myObject;
+            string ingredients = "";
+            string steps = ""; ;
+            foreach (var ingredient in rep.ingredient)
+            {
+                if (ingredient == rep.ingredient[rep.ingredient.Count - 1])
+                {
+                    ingredients += ingredient;
+                    break;
+                }
+                ingredients += ingredient + '+';
+            }
+            foreach (var step in rep.step)
+            {
+                if (step == rep.step[rep.step.Count - 1])
+                {
+                    steps += step;
+                    break;
+                }
+                steps += step + "+";
+            }
+
+            var url = WebUtility.UrlEncode(Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(rep.imageSource)));
+            await Shell.Current.GoToAsync($"{nameof(RecipeDetailPage)}?name={rep.name}&ingredient={ingredients}&step={steps}&imageSource={url}&numberPerson={rep.numberPerson}&cookingTime={rep.cookingTime}&difficulty={rep.difficulty}&likes={rep.likes}");
         }
     }
 }
